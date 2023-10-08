2f1a7a80aa122df635b27228b4ef366b90b4c5fd
sanchuanmingyue <118170955+sanchuanmingyue@users.noreply.github.com>
2023-10-08 22:04:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,8 +6,6 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-// 请将以下代码按照图示粘贴到对应位置
-USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx3cd7fc85287052c7',
 
@@ -22,9 +20,9 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '三三',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU25884Te7I4BPnjK5V3oX2DANEXxCaicglB4caG',
+      id: 'o1zz_6rOc-P-4aAe9Z-CxPMaE9xw	移除',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'nhKHXOA8MyhOvzlhL8aiJlYO7eIBjE-bNzaZPIl6CDQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       /*horoscopeDate: '12-27',
       festivals: [
@@ -52,7 +50,7 @@ USE_PASSAGE: 'push-deer',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'nhKHXOA8MyhOvzlhL8aiJlYO7eIBjE-bNzaZPIl6CDQ',
+  CALLBACK_TEMPLATE_ID: 'FbWZ97YvgmN2DoRvtMr3njyESpvfwyCk9harFEuZmNU',
 
   CALLBACK_USERS: [
     {
